5705d8fed2768fef21cee17d607bf0dfcb133686
Harsh Patle <harshpatle491@gmail.com>
2025-05-24 14:54:58 +0530

    final

--- kitsup-front/src/features/auth/authAPI.js
+++ kitsup-front/src/features/auth/authAPI.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const BASE_URL = "https://kitsup-backend-132946936245.asia-south1.run.app/api/auth";
 
 export const loginApi = (credentials) =>
   axios.post(`${BASE_URL}/login`, credentials);
