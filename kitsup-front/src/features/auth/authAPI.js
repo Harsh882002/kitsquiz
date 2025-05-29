8ae83b9b90fd33eb148ee95c0c42339fbc9e8736
Harsh Patle <harshpatle491@gmail.com>
2025-05-29 18:00:29 +0530

    deploy

--- kitsup-front/src/features/auth/authAPI.js
+++ kitsup-front/src/features/auth/authAPI.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = 'http://35.244.22.133:8080/api/auth';
+const BASE_URL = 'http://35.244.22.133:8080/api/auth/';
 
 // const BASE_URL = "http://localhost:8080/api/auth"; 
  
