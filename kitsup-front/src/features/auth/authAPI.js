1c9568e8015ae1d8e36541689e4103b40b8b6767
harsh <harshpatle491@gmailil.com>
2025-06-21 13:26:41 +0530

    changes

--- kitsup-front/src/features/auth/authAPI.js
+++ kitsup-front/src/features/auth/authAPI.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = 'https://backend-kitsquiz.onrender.com/api/auth/';
+const BASE_URL = 'https://backend-kitsquiz.onrender.com/api/auth';
 
 // const BASE_URL = "http://localhost:8080/api/auth";
 
